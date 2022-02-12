a77a6544a9c0fafcff1e11c47d3fdb2965748d12
Austyn <austyn.owens1@gmail.com>
2022-02-11 19:07:20 -0600

    removed inline javascript

--- advHigherLower.js
+++ advHigherLower.js
@@ -4,6 +4,7 @@ let guessList = [];
 let winningNumber = 0;
 let chosenMax = document.getElementById("chosenMax");
 let messageArea = document.getElementById("message");
+let guessBtn = document.getElementById("guessBtn");
 let $messageEffects =  $("#message");
 
 
@@ -19,6 +20,8 @@ chosenMax.innerHTML = maxNumber;
 winningNumber = Math.floor(Math.random() * maxNumber) + 1;
 
 console.log(winningNumber);
+
+guessBtn.onclick = processGuess;
 
 
 //FUNCTIONS
@@ -38,7 +41,7 @@ function processGuess() {
         updateMessage("That is not a number!");
 
     }
-    else if(guess > maxNumber){
+    else if(guess > maxNumber || guess <= 0){
 
         updateMessage("That number is not in range, try again.");
 
